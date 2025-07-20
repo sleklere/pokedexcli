c63bec3fe47ed0625848619f752c0222a2a20e8c
sleklere <santiagoleklere@gmail.com>
2025-07-19 21:32:14 -0300

    readline implemented for better cli experience

--- repl.go
+++ repl.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"io"
+	"strings"
 	"github.com/sleklere/pokedexcli/internal/pokeapi"
+
+	"github.com/chzyer/readline"
 )
 
 
@@ -16,33 +18,62 @@ type config struct {
 }
 
 func startRepl(config *config) {
-	scanner := bufio.NewScanner(os.Stdin)
+	supportedCommands := getCommands()
+
+	autoCompleteItems := make([]readline.PrefixCompleterInterface, 0)
+
+	for _, v := range supportedCommands {
+		autoCompleteItems = append(autoCompleteItems, readline.PcItem(v.name))
+	}
+
+
+	completer := readline.NewPrefixCompleter(
+		autoCompleteItems...
+	)
+
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:          "Pokedex > ",
+		HistoryFile:     "/tmp/pokedex_history",
+		AutoComplete:    completer,
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
 
 	for {
-		fmt.Print("Pokedex > ")
+		line, err := rl.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
 
-		scanner.Scan()
-		userInput := scanner.Text()
+		userInput := strings.TrimSpace(line)
+		if userInput == "exit" {
+			break
+		}
 
 		cleaned := cleanInput(userInput)
-
 		if len(cleaned) == 0 {
 			fmt.Println("Please enter a command")
 			continue
 		}
 
 		command := cleaned[0]
-
-		supportedCommands := getCommands()
-
 		supportedCommand, ok := supportedCommands[command]
 		if ok {
 			err := supportedCommand.callback(config, cleaned[1:]...)
 			if err != nil {
 				fmt.Printf("Error executing command '%s': %v\n", supportedCommand.name, err)
 			}
-		}
-		if !ok {
+		} else {
 			fmt.Println("Unknown command")
 		}
 	}
